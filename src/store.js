0ce971deebc7e22c23898023dba07abb8af4c561
unknown <nvn.bhargav@gmail.com>
2021-10-01 19:57:56 +0530

    - added Redux Dev tools

--- src/store.js
+++ src/store.js
@@ -1,9 +1,26 @@
 import { createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 import { todos } from "./todos/reducers";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 const reducers = {
   todos,
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
-export const configureStore = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    +window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+//window.__REDUX_DEVTOOLD_EXTENSION__ && window.__REDUX_DEVTOOLD_EXTENSION__()  connect our app to redux dev tools
